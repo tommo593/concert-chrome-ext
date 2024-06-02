fbe494199c432fbf251fb4dc8e09f0e5943c112e
tommo593 <tomdevwork@gmail.com>
2024-06-02 17:39:36 +0100

    Completed script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-async function fetchData() {}
+async function fetchData() {
+  const url =
+    "https://concerts-artists-events-tracker.p.rapidapi.com/location?name=London&minDate=2024-06-03&maxDate=2026-07-31&page=2";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "bbf3e67d10msh7c4eb28ad14f439p1080a2jsn6286e5617279",
+      "X-RapidAPI-Host": "concerts-artists-events-tracker.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+
+  document.getElementById("concerts").innerHTML = record.data.map((item) =>
+    `<li>${item.name}</li>`.join("")
+  );
+}
+
+fetchData();
